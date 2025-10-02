67bb59ceea25063497a64c253d6446ef302babfa
AbhijeetSRawat <rawatab2@gmail.com>
2025-10-02 14:54:09 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
 
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<Home/>}/>
       </Routes>
 
       <Footer/>
